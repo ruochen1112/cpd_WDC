0d42b84b6c4d17dbc2a759281a63305ec38235bd
Rachel Qiu <q.ruochen@gmail.com>
2019-03-06 15:12:07 -0600

    test

---
+++ Examples/OAuthProxyExample/config.js
@@ -0,0 +1,10 @@
+// The necessary configuration for your server
+// Contains credentials for your Foursquare application
+// And the new redirect path for the OAuth flow
+module.exports = {
+ 'HOSTPATH': 'http://localhost',
+ 'PORT': 3333,
+ 'CLIENT_ID': 'chicago_public_api',
+ 'CLIENT_SECRET': 'QZHLF9wyhHCTa{AEocHNffsBh',
+ 'REDIRECT_PATH': '/redirect'
+};
